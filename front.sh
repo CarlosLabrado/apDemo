6fe0e9649307f78eccbd0603cbd2fdcc3869f693
CarlosLabrado <carlos@petrolog.us>
2018-05-09 12:32:03 -0600

    port forward didn't worked

--- front.sh
+++ front.sh
@@ -6,8 +6,5 @@ export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 echo activate connection first
 python /usr/src/app/activate_connection.py
 
-echo port forward
-sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
-
 echo ..... start.sh: Running main app ....
 python /usr/src/app/front.py
